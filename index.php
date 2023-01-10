36505f169882b378090c019c7566383bc40af92d
Anas Nasr <102870259+anasn5020@users.noreply.github.com>
2023-01-10 18:41:59 +0500

    Changes

--- index.php
+++ index.php
@@ -160,7 +160,7 @@ else{
     ?>
 </div>
   <div class="container my-4">
-    <h2>Add a Note to iNotes</h2>
+    <h2>Add a Note</h2>
     <form action="/index.php" method="POST">
       <div class="form-group">
         <label for="title">Note Title</label>
